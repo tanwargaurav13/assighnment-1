762cdf616e87bd851904ed6cf1f4d1fcefd966a9
tanwargaurav13 <gauravtanwar713@gmail.com>
2025-08-07 21:08:23 +0530

    Add files via upload

---
+++ # program to take two numbers as input.py
@@ -0,0 +1,6 @@
+# program to take two numbers as input from the user 
+
+# taking input from the user 
+
+num1 = float (input("enter the first number: "))
+num2 = float (input("enter the second number: "))
---
+++ assignment1.py
@@ -0,0 +1,6 @@
+
+#Taking input  
+first_name = input ("Enter your first name:")
+last_name = input ("Enter your last name")
+
+print("\nHello,", first_name, last_name + "!")
